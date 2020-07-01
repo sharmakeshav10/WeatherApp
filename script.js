560da5b240915059f04817fcd3d366644ee96b72
sharmakeshav10 <46739493+sharmakeshav10@users.noreply.github.com>
2020-07-01 13:03:23 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let appId = "3191d3e9cd257c3f98697f4b4fa022bd";
+let units = "metric";
+let searchMethod = "q";
+
+function searchWeather(searchTerm) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
+}
+
+function init(resultFromServer) {
+    switch(resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('clear.jpg')";
+            break;
+
+        case 'Rain':
+        case 'Drizzle':  
+        case 'Mist':
+            document.body.style.backgroundImage = "url('rainy.jpg')";
+            break;
+        
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('cloudy.jpg')";
+            break;  
+            
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('stormy.jpg')";
+            break; 
+            
+        case 'Snow':
+            document.body.style.backgroundImage = "url('snowy.jpg')";
+            break;
+            
+        default:
+            break;
+    }
+    
+    let weatherHeader = document.getElementById("weatherHeader");
+    let temperatureElement = document.getElementById("temperature");
+    let humidityElement = document.getElementById("humidity");
+    let windSpeedElement = document.getElementById("windSpeed");
+    let cityHeader = document.getElementById("cityHeader");
+
+    let weatherIcon = document.getElementById("iconImg");
+    weatherIcon.src = "http://openweathermap.org/img/w/" + resultFromServer.weather[0].icon + ".png";
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + "&#176";
+    humidityElement.innerHTML = "Humidity levels at " + resultFromServer.main.humidity + '%';
+    windSpeedElement.innerHTML = "Winds at " + Math.floor(resultFromServer.wind.speed) + " m/s";
+    cityHeader.innerHTML = resultFromServer.name;
+    
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.5}px)`;
+    weatherContainer.style.visibility = "visible";
+}
+
+
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm) {
+        searchWeather(searchTerm);
+    }
+})
